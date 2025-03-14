7fcf7c39d26f1195a248b98ad1c088c908615ffc
Dishats <155805719+Dishats@users.noreply.github.com>
2025-03-14 21:54:40 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { assets } from '../assets/assetss'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>
+          CONTACT <span className='text-gray-800 font-semibold'>US</span>
+        </p>
+      </div>
+      <div>
+        <img src={assets.contact_image} alt="" />
+        <div>
+          <p>Our OFFICE</p>
+          <p>571403 PES College of Engineering <br /> Mandya ,Karnataka</p>
+          <p> TEL : 0000000000 <br />Email:sample@gmail.com</p>
+          <p>Careers at CARE-CONNECT</p>
+          <p>contact us to prioritize your health</p>
+          <button>Explore more</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
